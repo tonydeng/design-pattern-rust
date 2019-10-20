19f217083a744f80142ec148b9c41bb9875c5b5f
Tony Deng <wolf.deng@gmail.com>
2019-10-21 00:14:06 +0800

    :bug: fix(1.0): 解决测试的目录和结构问题
    Signed-off-by: Tony Deng <wolf.deng@gmail.com>

---
+++ factory/tests/simple_test.rs
@@ -0,0 +1,22 @@
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(3,add_one(2));
+    }
+}
+
+
+// fn create_product_a() {
+//     let factory_a = create_factory(FactoryID::A);
+//     let a_x = factory_a.create_product_x();
+//     let a_y = factory_a.create_product_y();
+
+//     println!("{}",a_x.get_value());
+// }
